e33451f3ac4a397bd36a7a9c73504f02071e949f
Kanahiro <mediterranean1769@gmail.com>
2021-06-12 22:52:03 +0900

    prototype

---
+++ browser_item.py
@@ -0,0 +1,176 @@
+from datetime import timedelta
+import os
+
+import sip
+from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction
+from qgis.core import *
+
+from .nowcast_data_loader import NowcastDataLoader
+
+# The following long string is to activate temporal controller settings of QgsRasterLayer,
+# Over-writing props in QgsMapLayerTemporalProperties doesn't work correctly.
+# Seting with QML file works good, so write temporal QML file and load it.
+qml_string = r"""
+<!DOCTYPE qgis PUBLIC 'http://mrcc.com/qgis.dtd' 'SYSTEM'>
+<qgis styleCategories="AllStyleCategories" maxScale="0" version="3.16.7-Hannover" hasScaleBasedVisibilityFlag="0" minScale="1e+08">
+  <flags>
+    <Identifiable>1</Identifiable>
+    <Removable>1</Removable>
+    <Searchable>1</Searchable>
+  </flags>
+  <temporal enabled="1" fetchMode="0" mode="0">
+    <fixedRange>
+      <start>{START_DATETIME}</start>
+      <end>{END_DATETIME}</end>
+    </fixedRange>
+  </temporal>
+  <customproperties>
+    <property key="WMSBackgroundLayer" value="false"/>
+    <property key="WMSPublishDataSourceUrl" value="false"/>
+    <property key="embeddedWidgets/count" value="0"/>
+    <property key="identify/format" value="Undefined"/>
+  </customproperties>
+  <pipe>
+    <provider>
+      <resampling enabled="false" zoomedInResamplingMethod="nearestNeighbour" zoomedOutResamplingMethod="nearestNeighbour" maxOversampling="2"/>
+    </provider>
+    <rasterrenderer band="1" opacity="1" nodataColor="" type="singlebandcolordata" alphaBand="-1">
+      <rasterTransparency/>
+      <minMaxOrigin>
+        <limits>None</limits>
+        <extent>WholeRaster</extent>
+        <statAccuracy>Estimated</statAccuracy>
+        <cumulativeCutLower>0.02</cumulativeCutLower>
+        <cumulativeCutUpper>0.98</cumulativeCutUpper>
+        <stdDevFactor>2</stdDevFactor>
+      </minMaxOrigin>
+    </rasterrenderer>
+    <brightnesscontrast gamma="1" contrast="0" brightness="0"/>
+    <huesaturation grayscaleMode="0" colorizeRed="255" saturation="0" colorizeOn="0" colorizeGreen="128" colorizeBlue="128" colorizeStrength="100"/>
+    <rasterresampler maxOversampling="2"/>
+    <resamplingStage>resamplingFilter</resamplingStage>
+  </pipe>
+  <blendMode>0</blendMode>
+</qgis>
+"""
+
+
+def make_raster_layer(tiledata, animation=False):
+    rlayer = QgsRasterLayer(
+        'type=xyz&url=' + tiledata['url'] + '&zmax=10&zmin=4', tiledata['datetime'].strftime(r'%Y-%m-%d %H:%M'), 'wms')
+
+    if animation:
+        start_datetime = tiledata['datetime']
+        end_datetime = tiledata['datetime'] + timedelta(minutes=4, seconds=59)
+        import tempfile
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_qml = os.path.join(temp_dir, 'temp.qml')
+            with open(temp_qml, mode='w') as f:
+                print(qml_string.replace(r'{START_DATETIME}', start_datetime.strftime(
+                    r'%Y-%m-%dT%H:%M:%SZ')).replace(r'{END_DATETIME}', end_datetime.strftime(r'%Y-%m-%dT%H:%M:%SZ')))
+                f.write(qml_string.replace(r'{START_DATETIME}', start_datetime.strftime(
+                    r'%Y-%m-%dT%H:%M:%SZ')).replace(r'{END_DATETIME}', end_datetime.strftime(r'%Y-%m-%dT%H:%M:%SZ')))
+            rlayer.loadNamedStyle(temp_qml)
+
+    return rlayer
+
+
+class DataItemProvider(QgsDataItemProvider):
+    def __init__(self):
+        QgsDataItemProvider.__init__(self)
+
+    def name(self):
+        return "NowcastProvider"
+
+    def capabilities(self):
+        return QgsDataProvider.Net
+
+    def createDataItem(self, path, parentItem):
+        root = RootCollection()
+        sip.transferto(root, None)
+        return root
+
+
+class RootCollection(QgsDataCollectionItem):
+
+    def __init__(self):
+        QgsDataCollectionItem.__init__(
+            self, None, "NowcastTool", "/NowcastTool")
+
+        self.past_tiledata_list = []
+        self.forecast_tiledata_list = []
+
+        self.reload()
+
+    def createChildren(self):
+        children = []
+
+        for tiledata in self.past_tiledata_list + self.forecast_tiledata_list:
+            item = TileDataItem(self, tiledata)
+            sip.transferto(item, self)
+            children.append(item)
+        return children
+
+    def reload(self):
+        loader = NowcastDataLoader()
+        self.past_tiledata_list, self.forecast_tiledata_list = loader.fetch_nowcast_timedata()
+        self.refreshConnections()
+
+    def actions(self, parent):
+        actions = []
+
+        reload_action = QAction(QIcon(), 'タイル情報再取得', parent)
+        reload_action.triggered.connect(self.reload)
+        actions.append(reload_action)
+
+        add_all_as_animation_action = QAction(QIcon(), 'アニメーション表示', parent)
+        add_all_as_animation_action.triggered.connect(
+            self.add_all_as_animation_action)
+        actions.append(add_all_as_animation_action)
+
+        config_action = QAction(QIcon(), '設定', parent)
+        config_action.triggered.connect(lambda: print('a'))
+        actions.append(config_action)
+
+        return actions
+
+    def add_all_as_animation_action(self):
+        root = QgsProject().instance().layerTreeRoot()
+        node_map = root.insertGroup(0, 'Nowcast')
+        node_map.setExpanded(False)
+
+        for tiledata in self.past_tiledata_list + self.forecast_tiledata_list:
+            tile_rlayer = make_raster_layer(tiledata, animation=True)
+            QgsProject.instance().addMapLayer(tile_rlayer, False)
+            node_map.addLayer(tile_rlayer)
+
+
+class TileDataItem(QgsDataItem):
+    def __init__(self, parent, tiledata):
+        QgsDataItem.__init__(self, QgsDataItem.Custom,
+                             parent,
+                             tiledata['datetime'].strftime('%Y-%m-%d %H:%M'),
+                             "/NowcastTool/item/" + tiledata['datetime'].strftime(r'%Y-%m-%d %H:%M'))
+
+        self.populate()  # set to treat Item as not-folder-like
+
+        self.__tiledata = tiledata
+
+    def handleDoubleClick(self):
+        self.add_layer()
+        return True
+
+    def actions(self, parent):
+        actions = []
+
+        add_action = QAction(QIcon(), 'マップへ追加', parent)
+        add_action.triggered.connect(lambda: self.add_layer())
+        actions.append(add_action)
+
+        return actions
+
+    def add_layer(self):
+        rlayer = make_raster_layer(self.__tiledata)
+        QgsProject.instance().addMapLayer(rlayer)
---
+++ nowcast_data_loader.py
@@ -0,0 +1,97 @@
+import json
+from datetime import datetime, timedelta, timezone
+
+from PyQt5.QtNetwork import QNetworkRequest, QNetworkReply
+from PyQt5.QtCore import QUrl, QEventLoop, QTextStream
+from qgis.core import QgsNetworkAccessManager
+
+
+class NowcastDataLoader:
+    def __init__(self):
+        self.eventloop = None
+        self.isRepliedN1 = False
+        self.isRepliedN2 = False
+
+    def fetch_nowcast_timedata(self):
+        n1 = 'https://www.jma.go.jp/bosai/jmatile/data/nowc/targetTimes_N1.json'
+        n2 = 'https://www.jma.go.jp/bosai/jmatile/data/nowc/targetTimes_N2.json'
+
+        networkAccessManager = QgsNetworkAccessManager.instance()
+
+        self.eventLoop = QEventLoop()
+        req1 = QNetworkRequest(QUrl(n1))
+        req2 = QNetworkRequest(QUrl(n2))
+        reply1 = networkAccessManager.get(req1)
+        reply2 = networkAccessManager.get(req2)
+        reply1.finished.connect(self.gotReplyFromN1)
+        reply2.finished.connect(self.gotReplyFromN2)
+        self.eventLoop.exec_()
+
+        past_timedata_list = self.jsonify(reply1)
+        forecast_timedata_list = self.jsonify(reply2)
+
+        past_tiledata_list = list(
+            map(self.get_tiledata_from, past_timedata_list))
+        forecast_tiledata_list = list(
+            map(self.get_tiledata_from, forecast_timedata_list))
+
+        extended_tiledata_list = self.make_extended_tiledata_list(
+            past_tiledata_list[0])
+
+        return extended_tiledata_list + past_tiledata_list, forecast_tiledata_list
+
+    def gotReplyFromN1(self):
+        self.isRepliedN1 = True
+        if self.isRepliedN2:
+            self.eventLoop.quit()
+
+    def gotReplyFromN2(self):
+        self.isRepliedN2 = True
+        if self.isRepliedN1:
+            self.eventLoop.quit()
+
+    @ staticmethod
+    def make_extended_tiledata_list(oldest_past_tiledata):
+        oldest_datetime = oldest_past_tiledata['datetime']
+
+        def make_tiledata(jst_dt: datetime):
+            utc_dt = jst_dt - timedelta(hours=9)
+            yyyymmddhhmmss = f'{str(utc_dt.year)}{str(utc_dt.month).zfill(2)}{str(utc_dt.day).zfill(2)}{str(utc_dt.hour).zfill(2)}{str(utc_dt.minute).zfill(2)}{str(utc_dt.second).zfill(2)}'
+            return {
+                'datetime': jst_dt,
+                'url': f'https://www.jma.go.jp/bosai/jmatile/data/nowc/{yyyymmddhhmmss}/none/{yyyymmddhhmmss}/surf/hrpns/' + r'{z}/{x}/{y}.png'
+
+            }
+        extend_counter = 120
+        extended_tiledata_list = [make_tiledata(
+            oldest_datetime - timedelta(minutes=(extend_counter - i) * 5)) for i in range(1, extend_counter)]
+        return extended_tiledata_list
+
+    @ staticmethod
+    def jsonify(reply: QNetworkReply):
+        json_str = QTextStream(reply).readAll()
+        json_dict = json.loads(json_str)
+        return sorted(json_dict, key=lambda x: x['validtime'])
+
+    @staticmethod
+    def get_tiledata_from(timedata: dict):
+        baseurl = r'https://www.jma.go.jp/bosai/jmatile/data/nowc/{basetime}/none/{validtime}/surf/hrpns/{z}/{x}/{y}.png'
+        tileurl = baseurl.replace(r'{basetime}', timedata['basetime']).replace(
+            r'{validtime}', timedata['validtime'])
+
+        def text_to_datetime(yyyymmddhhmmss: str):
+            year = int(yyyymmddhhmmss[0:4])
+            month = int(yyyymmddhhmmss[4:6])
+            day = int(yyyymmddhhmmss[6:8])
+            hour = int(yyyymmddhhmmss[8:10])
+            minute = int(yyyymmddhhmmss[10:12])
+            second = int(yyyymmddhhmmss[12:14])
+            jst_dt = datetime(
+                year, month, day, hour, minute, second, tzinfo=timezone.utc).astimezone(timezone(timedelta(hours=9)))
+
+            return jst_dt
+
+        return {
+            'datetime': text_to_datetime(timedata['validtime']),
+            'url': tileurl
+        }
